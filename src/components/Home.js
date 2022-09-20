9168f47f23e11fd99a26835d0d5eac0680434496
unknown <dc3016@srmist.edu.in>
2022-09-20 21:41:26 +0530

    home page done

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <div className=' '>
         <p className=" font-normal text-[20px]  text-center text-[#1B31A8] ">Let's calculate <span className=' font-extrabold'>distance</span> from Google maps</p>
       </div>
-      <div className=' flex flex-row'>
+      <div className=' flex flex-row mt-[33px] ml-[156px] mr-[104px]'>
         <div className=' md:w-1/2 '>
         <div className="flex flex-col  w-[100%] h-[100%] rounded-br-xl " id="bg-form">
                 <label className="">Source</label>
@@ -17,6 +17,9 @@ const Home = () => {
                       placeholder="Source"
                       value=""
                     />
+                    <button className=" rounded-3xl ml-[280px] w-[141px] text-white p-3  bg-[#1B31A8] ">
+                  Calculate
+                </button>
                     <label className="">Destination</label>
                     <input
                       className=""
@@ -24,15 +27,17 @@ const Home = () => {
                       placeholder="Destination"
                       value=""
                     />
-                    <button
-                  className=""
-                 
-                >
-                  Add Spend
-                </button>
+                    <label className="">Distance</label>
+                    <input
+                      className=""
+                      type="text"
+                      placeholder="Distance"
+                      value=""
+                    />
+                    
                 </div>
         </div>
-        <div className='bg-[#E5E5E5] md:w-1/2'>
+        <div className='bg-[#E5E5E5] md:w-1/2 '>
             <div>
                 
             </div>
